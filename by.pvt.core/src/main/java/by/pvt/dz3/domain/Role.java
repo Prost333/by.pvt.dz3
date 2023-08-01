345a6b2586d86ca7f7d99e15d97b049f24f60a67
prost333 <dimabolshoy333@yandex.by>
2023-08-01 14:35:56 +0300

    правка

--- by.pvt.core/src/main/java/by/pvt/dz3/domain/Role.java
+++ by.pvt.core/src/main/java/by/pvt/dz3/domain/Role.java
@@ -1,11 +1,5 @@
 package by.pvt.dz3.domain;
 
 public enum Role {
-    ADMIN("Администратор"),
-    CLIENT("Клиент");
-    String name;
 
-    Role(String name) {
-        this.name = name;
-    }
 }
